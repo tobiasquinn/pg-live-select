19413fa64812656fbcb4f06c9e0e51e9f2e897f6
Ben Green <ben@latenightsketches.com>
2015-11-12 17:01:03 -0800

    Travis CI updated

--- test/settings.js
+++ test/settings.js
@@ -14,3 +14,13 @@ module.exports = {
     serverId: 349
   }
 }
+
+if(process.env.TRAVIS) {
+  // Provide modified connection settings for Travis CI
+  module.exports.pg.connStr = 'postgres://postgres@127.0.0.1/travis_ci_test';
+  module.exports.pg.channel = 'travis_channel';
+
+  module.exports.my.password = '';
+  module.exports.my.port = process.env.TEST_MYSQL_PORT * 1;
+  module.exports.my.database = 'travis_test';
+}
---
+++ test/travis/runner.sh
@@ -0,0 +1,14 @@
+echo -e "\033[1;35m Run test on Postgres\033[0m"
+psql -c 'create database travis_ci_test;' -U postgres
+npm run test-pg
+
+echo -e "\033[1;35m Run test on MySQL 5.1.73\033[0m"
+mysql -hlocalhost -p3351 -uroot -e "create database travis_test;"
+TEST_MYSQL_PORT=3351 npm run test-mysql || exit $?
+echo -e "\033[1;35m Run test on MySQL 5.5.41\033[0m"
+mysql -hlocalhost -p3355 -uroot -e "create database travis_test;"
+TEST_MYSQL_PORT=3355 npm run test-mysql || exit $?
+echo -e "\033[1;35m Run test on MySQL 5.6.22\033[0m"
+mysql -hlocalhost -p3356 -uroot -e "create database travis_test;"
+TEST_MYSQL_PORT=3356 npm run test-mysql || exit $?
+
